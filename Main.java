6c49c82f1838087f60f9979e47b03f1c9c31941b
Luc Jaubert <lucjaubert@gmail.com>
2023-03-16 11:33:25 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bank account = new Bank(1000);
+
+        Transactions.withdraw(account, 100, new TransactionListener() {
+            @Override
+            public void onComplete(Bank account) {
+                System.out.printf("Your balance is: %d%n", account.getTotal());
+            }
+        });
+    }
+}
+
